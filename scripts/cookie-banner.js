9d6ad346cb299deb9f35c8e4e21112a38cf34ea2
Amy Yi Li <142811190+AmyLi12@users.noreply.github.com>
2023-11-01 14:34:49 +0000

    added javascript for accept cookies button

---
+++ scripts/cookie-banner.js
@@ -0,0 +1,8 @@
+// when #accept-cookies-button is clicked:
+$("#accept-cookies-button").click(function() {
+
+    console.log("button clicked");
+
+    // add .hidden to #cookie-banner
+    $("#cookie-banner").addClass("hidden");
+  });
